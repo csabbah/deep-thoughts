39b7a6ec9fb1562931e08b97cb9e0670f315d33b
Csabbah <csabbah@ryerson.ca>
2022-07-05 14:17:18 -0400

    Added mutations for Thoughts, Reactions and Friends

--- .history/Server/schemas/resolvers_20220705140736.js
+++ .history/Server/schemas/resolvers_20220705140736.js
@@ -106,19 +106,18 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
 
-    addFriend: async (parent, { friendId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { friends: friendId } },
-          { new: true }
-        ).populate('friends');
-
-        return updatedUser;
-      }
-
-      throw new AuthenticationError('You need to be logged in!');
-    },
+    mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+  addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+    _id
+    reactionCount
+    reactions {
+      _id
+      reactionBody
+      createdAt
+      username
+    }
+  }
+}
   },
 };
 
