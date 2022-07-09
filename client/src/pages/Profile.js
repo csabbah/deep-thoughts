29055ef7f9f526ddcd96f866c5b1f3ff735201b2
Csabbah <csabbah@ryerson.ca>
2022-07-09 09:56:43 -0400

    Logged in users can now add friends when visiting other user profile pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,14 +4,18 @@ import FriendList from '../components/FriendList';
 
 import ThoughtList from '../components/ThoughtList';
 
-import { useQuery } from '@apollo/client';
-
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
 import { Navigate, useParams } from 'react-router-dom';
 
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
+
 const Profile = () => {
+  // destructuure the mutation function from ADD_FRIEND
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   const { username: userParam } = useParams();
 
   // When loading the data, if userParam exists, that means we are querying other users (endpoint params)
@@ -42,8 +46,27 @@ const Profile = () => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="flex-row justify-space-between mb-3">
+      <h2 className="bg-dark text-secondary p-3 display-inline-block">
+        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      </h2>
+
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+      )}
       <div className="col-12 mb-3 col-lg-8">
         <ThoughtList
           thoughts={user.thoughts}
