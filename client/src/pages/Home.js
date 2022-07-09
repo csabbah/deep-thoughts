e39b7c4273b9d67713262a96eae3976ce1071bac
Csabbah <csabbah@ryerson.ca>
2022-07-09 08:53:45 -0400

    Users can now view their individualized data including their thoughts, reactions and friends list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ThoughtList from '../components/ThoughtList';
 
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+
+import FriendList from '../components/FriendList';
+
+import Auth from '../utils/auth';
 
 const Home = () => {
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
+  const loggedIn = Auth.loggedIn();
+
   // use useQuery hook to make query request
   const { loading, data } = useQuery(QUERY_THOUGHTS);
 
@@ -13,7 +22,7 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
@@ -24,6 +33,15 @@ const Home = () => {
           )}
         </div>
       </div>
+      {loggedIn && userData ? (
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={userData.me.username}
+            friendCount={userData.me.friendCount}
+            friends={userData.me.friends}
+          />
+        </div>
+      ) : null}
     </main>
   );
 };
